22ab897ac21b9bb03170444840172db563318311
rejohnbd <rejohn.cse@gmail.com>
2023-10-14 02:42:51 +0600

    Remove php-mqtt laravel-client code from AppServiceProvider

--- apparel-front/app/Providers/AppServiceProvider.php
+++ apparel-front/app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-// use PhpMqtt\Client\Facades\MQTT;
-// use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,13 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-
-        // Log::info('ok');
-        // $mqtt = MQTT::connection();
-        // $mqtt->subscribe('#', function (string $topic, string $message) {
-        //     Log::info('topic :', $message);
-        //     Log::info('message :', $topic);
-        // }, 1);
-        // $mqtt->loop(true);
     }
 }
