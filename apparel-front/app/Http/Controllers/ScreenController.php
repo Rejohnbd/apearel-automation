a36554a907d8537220c1cf439fb7df57b5e03270
rejohnbd <rejohn.cse@gmail.com>
2023-10-13 15:38:00 +0600

    Add laravel mqtt-client

--- apparel-front/app/Http/Controllers/ScreenController.php
+++ apparel-front/app/Http/Controllers/ScreenController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpMqtt\Client\Facades\MQTT;
 use Inertia\Inertia;
 use Inertia\Response;
 
+
 class ScreenController extends Controller
 {
-    public function first_flood(): Response
+    public function first_floor(): Response
     {
+        MQTT::publish('/some/topic', 'Hello World! Rejohn');
         return Inertia::render('Screen');
     }
 }
--- apparel-front/app/Providers/AppServiceProvider.php
+++ apparel-front/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use PhpMqtt\Client\Facades\MQTT;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Log::info('ok');
+        // MQTT
+        $mqtt = MQTT::connection();
+        $mqtt->subscribe('#', function (string $topic, string $message) {
+            Log::info('topic :', $message);
+            Log::info('message :', $topic);
+        }, 1);
+        // Log::info('mqtt', $mqtt);
+        // $mqtt->loop(true);
     }
 }
