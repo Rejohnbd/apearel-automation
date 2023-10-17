3f3663d0636eb22673346a89f6c74ebb0c511247
rejohnbd <rejohn.cse@gmail.com>
2023-10-18 01:58:49 +0600

    Add paho-mqtt and run in flask

--- py-app/app.py
+++ py-app/app.py
@@ -1,10 +1,27 @@
 from flask import Flask
+import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
+
+def on_connect(client, userdata, flags, rc):
+    # This will be called once the client connects
+    print(f"Connected with result code {rc}")
+    # Subscribe here!
+    client.subscribe("first-floor")
+
+def on_message(client, userdata, msg):
+    print(f"Message received [{msg.topic}]: {msg.payload}")
 
 @app.route("/")
 def welcome():
     return "<p>App Running</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    client = mqtt.Client() 
+    # client.on_connect = on_connect
+    # client.on_message = on_message
+    # client.username_pw_set("myusername", "password")
+    client.connect('127.0.0.1', 1883)
+    client.loop_start()
+
+    app.run(debug=True, host='127.0.0.1', port=5000)
