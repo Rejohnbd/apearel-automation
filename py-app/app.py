59b35de5f4baf683db074d6f16ca197b0f903090
rejohnbd <rejohn.cse@gmail.com>
2023-10-18 14:23:18 +0600

    Add api request on mqtt request

--- py-app/app.py
+++ py-app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import paho.mqtt.client as mqtt
+import json
+import requests
 
 app = Flask(__name__)
 
@@ -10,7 +12,21 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("first-floor")
 
 def on_message(client, userdata, msg):
-    print(f"Message received [{msg.topic}]: {msg.payload}")
+    # print(f"Message received [{msg.topic}]: {msg.payload}")
+    payload = json.loads(msg.payload)
+    print(msg)
+    # deviceId = payload['deviceId']
+    # print(deviceId)
+    url = "http://127.0.0.1:8000/api/test-api"
+    data = {
+        'deviceId': payload['deviceId'],
+        'quantity': payload['quantity'],
+        'type': payload['type']
+    }
+
+    response = requests.post(url, json=data)
+    print("Status Code", response.status_code)
+    print("JSON Response ", response.json())
 
 @app.route("/")
 def welcome():
@@ -18,10 +34,10 @@ def welcome():
 
 if __name__ == '__main__':
     client = mqtt.Client() 
-    # client.on_connect = on_connect
-    # client.on_message = on_message
-    # client.username_pw_set("myusername", "password")
+    client.on_connect = on_connect
+    client.on_message = on_message
     client.connect('127.0.0.1', 1883)
     client.loop_start()
+    # client.username_pw_set("myusername", "password")
 
     app.run(debug=True, host='127.0.0.1', port=5000)
